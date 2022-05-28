6721e085c214df36f13b9cf60134c432a1b46683
caleb-freitas <calebfreitas@tutanota.com>
2022-05-27 21:32:04 -0300

    chore: added bull setup with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TweetsModule } from './tweets/tweets.module';
+import { MailingModule } from './mailing/mailing.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), TweetsModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: 'redis',
+        port: 6379,
+      },
+    }),
+    TweetsModule,
+    MailingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -3,9 +3,13 @@ import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
 import { PrismaService } from 'src/config/prisma.service';
 import { TweetsCountService } from './tweets-count/tweets-count.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [
+    CacheModule.register(),
+    BullModule.registerQueue({ name: 'emails' }),
+  ],
   controllers: [TweetsController],
   providers: [TweetsService, PrismaService, TweetsCountService],
 })
